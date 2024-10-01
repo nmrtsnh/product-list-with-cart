0e8e4b364978385561ae65a917c267d411a6793d
nmrtsnh <namratasinha26@gmail.com>
2024-10-01 15:30:06 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Product from "./components/product";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+         Desserts Waffle with Berries Waffle 6.50 Add to{" "}
+        Cart Vanilla Bean Crème
+    Brûlée Crème Brûlée 7.00 Add to Cart Macaron Mix of Five Macaron 8.00 Add to
+    Cart Classic Tiramisu Tiramisu 5.50 Add to Cart Pistachio Baklava Baklava
+    4.00 Add to Cart Lemon Meringue Pie Pie 5.00 Add to Cart Red Velvet Cake
+    Cake 4.50 Add to Cart Salted Caramel Brownie Brownie 4.50 Add to Cart
+    Vanilla Panna Cotta Panna Cotta 6.50 Add to Cart Your Cart (<!-- Quantity -->)
+    Your added items will appear here 
+        <div className="attribution">
+          Challenge by
+          <a href="https://www.frontendmentor.io?ref=challenge">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="">Your Name Here</a>.
+        </div>
+      </header> */}
+      <Product />
     </div>
   );
 }
---
+++ src/components/product.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const product = () => {
+  return (
+    <div>
+      <h1>Desserts</h1>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default product;
